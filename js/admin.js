356f93277905c9c75fa24e465504f973429a9a06
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-02-10 14:39:50 +0530

    .

--- js/admin.js
+++ js/admin.js
@@ -1,15 +1,15 @@
 window.addEventListener("load",function(){
     displayData();
         //Adding event to logo button
-        var home=document.getElementById("home").addEventListener("click",function(){ window.location.href="./pages/admin.html"; });
+        var home=document.getElementById("home").addEventListener("click",function(){ window.location.href="admin.html"; });
     
         //Adding Event to Header button
-        var add_category=document.getElementById("add_category").addEventListener("click",function(){ window.location.href="./pages/Addcategory.html"; });
-        var add_type=document.getElementById("add_type").addEventListener("click",function(){ window.location.href="./pages/Addtype.html"; });
-        var add_subtype=document.getElementById("add_subtype").addEventListener("click",function(){ window.location.href="./pages/Addsubtype.html"; });
-        var add_category=document.getElementById("add_category").addEventListener("click",function(){ window.location.href="./pages/Addcategory.html"; });
-        var add_brand=document.getElementById("add_brand").addEventListener("click",function(){ window.location.href="./pages/Addbrand.html"; });
-        var add_product=document.getElementById("add_product").addEventListener("click",function(){ window.location.href="./pages/Addproduct.html"; });
+        var add_category=document.getElementById("add_category").addEventListener("click",function(){ window.location.href="Addcategory.html"; });
+        var add_type=document.getElementById("add_type").addEventListener("click",function(){ window.location.href="Addtype.html"; });
+        var add_subtype=document.getElementById("add_subtype").addEventListener("click",function(){ window.location.href="Addsubtype.html"; });
+        var add_category=document.getElementById("add_category").addEventListener("click",function(){ window.location.href="Addcategory.html"; });
+        var add_brand=document.getElementById("add_brand").addEventListener("click",function(){ window.location.href="Addbrand.html"; });
+        var add_product=document.getElementById("add_product").addEventListener("click",function(){ window.location.href="Addproduct.html"; });
     
 });
 
